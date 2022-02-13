312a4f1fb5e1b3b673dfe6d5fc2014a6a09ad51d
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-02-13 11:26:39 -0300

    loading entities on server start config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
+import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection, getConnectionOptions } from 'typeorm';
+
 @Module({
-  imports: [CustomerModule],
+  imports: [
+    CustomerModule,
+    ProductModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+        }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
