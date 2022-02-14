3868fde604fa6bdc07433e3b01156f81e559cdd2
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-02-14 16:41:16 -0300

    migraiton for customers table

---
+++ src/migration/1644852348218-createCustomers.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createCustomers1644852348218 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'customers',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'customerAddress',
+            type: 'text',
+          },
+          {
+            name: 'cpf',
+            type: 'int',
+          },
+          {
+            name: 'cellphone',
+            type: 'int',
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('customers');
+  }
+}
